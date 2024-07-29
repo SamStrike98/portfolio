12edeaa142a7a1d3d0cfbdcc5449f6bb6de6c6a9
Sam Strike <sam.strike1@gmail.com>
2024-07-29 15:55:58 +0100

    Added links to live projects and repos.

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -8,51 +8,57 @@ import { FaMobileAlt } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { ImCross } from "react-icons/im";
 import { BsInfoLg } from "react-icons/bs";
+import Link from 'next/link';
 
 
 
-const ProjectCard = ({ title, image, description, mobileImage, techUsed}) => {
-    const [view, setView] = useState('pc')
-    const [isOpen, setIsOpen] = useState(false)
-    const [imageHeight, setImageHeight] = useState(500)
-    const id = title.trim() 
+const ProjectCard = ({ title, image, description, mobileImage, techUsed, repo, liveSite }) => {
+  const [view, setView] = useState('pc')
+  const [isOpen, setIsOpen] = useState(false)
+  const [imageHeight, setImageHeight] = useState(500)
+  const id = title.trim()
 
   return (
 
     <div id={id} className={`overflow-hidden ${isOpen ? 'h-[550px] w-[80%] project_card' : 'project_card_reverse h-[50px] w-3/4 lg:w-1/3'} border border-white border-opacity-30 bg-gray-800 bg-opacity-45 rounded-md p-4 flex flex-col items-center mb-8 `}>
-        <div className={`w-full flex flex-row justify-between mb-4 ${isOpen ? '' : 'h-full items-center'}`}>
-            <div>{''}</div>
+      <div className={`w-full flex flex-row justify-between mb-4 ${isOpen ? '' : 'h-full items-center'}`}>
+        <div>{''}</div>
 
-            <div className='flex flex-col justify-center items-center'>
-              <h2 className={`transition-all duation-500 delay-1000 text-white font-extrabold ${isOpen ? 'text-xl md:text-2xl decoration-green-400 underline-offset-8' : 'text-md lg:text-xl xl:text-2xl'}`}>{title}</h2>
-              <div className={`border-b border-green-400 transition-[width] duration-500 delay-500  ${isOpen ? 'w-[80%]' : 'w-0'}`}>{''}</div>
-            </div>
-            
-            <div className='text-white cursor-pointer' onClick={() => {setIsOpen(!isOpen) }}>{isOpen ? <ImCross color='red'/> : <GiHamburgerMenu color='#43a047'/>}</div>
+        <div className='flex flex-col justify-center items-center'>
+          <h2 className={`transition-all duation-500 delay-1000 text-white font-extrabold ${isOpen ? 'text-xl md:text-2xl decoration-green-400 underline-offset-8' : 'text-md lg:text-xl xl:text-2xl'}`}>{title}</h2>
+          <div className={`border-b border-green-400 transition-[width] duration-500 delay-500  ${isOpen ? 'w-[80%]' : 'w-0'}`}>{''}</div>
         </div>
 
-        <div className={`flex flex-col items-center`}>
-            { view === 'pc' ? <div className='w-5/6 h-[400px] overflow-hidden rounded-md'><Image alt={`${title} image`} onLoad={(e) => setImageHeight(e.target.clientHeight)} src={image} width={1000} height={1000} className={`transform hover:transform ${imageHeight > 400 ? 'hover:-translate-y-[calc(100%-400px)]' : ''} transition-transform duration-[5000ms] ease-linear`}/></div> : ''}
-            { view === 'info' ? <div className='w-5/6 h-[400px]'>
-                                  <h4 className='font-bold text-white mb-2 sm:mb-4 text-xl'>Tech Used:</h4>
-                                  <ul className='flex flex-row justify-evenly text-white mb-4 flex-wrap gap-1 sm:gap-2 '>
-                                    {techUsed.map(item => (
-                                      <li key={item} className='bg-opacity-45 ring-2 ring-inset p-3 rounded-md ring-green-400 ring-opacity-65 mb-4 text-xs md:text-md lg:text-lg'>{item}</li>
-                                    ))}
-                                  </ul>
-                                  <h4 className='font-bold text-white mb-2 sm:mb-4 text-xl'>Description:</h4>
-                                  <div className='h-[200px] overflow-auto p-1'>
-                                    <p className={`text-white text-xs sm:text-sm leading-normal md:text-md md:leading-loose min-`}>{description}</p>
-                                  </div>
-                                  
-                                </div> : ''}
+        <div className='text-white cursor-pointer' onClick={() => { setIsOpen(!isOpen) }}>{isOpen ? <ImCross color='red' /> : <GiHamburgerMenu color='#43a047' />}</div>
+      </div>
 
-            <div className='w-full flex flex-row justify-evenly mt-3'>
-              <div className={`rounded-full bg-black ${view === 'pc' ? 'bg-opacity-45 ring-2 ring-inset ring-green-400 ring-opacity-65' : 'bg-opacity-0'} p-3 transition-opacity`}> <IoIosDesktop size={35} color='white' className='cursor-pointer' onClick={() => setView('pc')}/> </div> 
-              {mobileImage ? <div className={`rounded-full bg-black ${view === 'mobile' ? 'bg-opacity-45 ring-2 ring-inset ring-green-400 ring-opacity-65' : 'bg-opacity-0'} p-3 transition-opacity`}> <FaMobileAlt size={42} color='white' className='cursor-pointer' onClick={() => setView('mobile')}/> </div> : ''}
-              <div className={`rounded-full bg-black ${view === 'info' ? 'bg-opacity-45 ring-2 ring-inset ring-green-400 ring-opacity-65' : 'bg-opacity-0'} p-3 transition-opacity`}> <BsInfoLg size={35} color='white' className='cursor-pointer' onClick={() => setView('info')}/> </div>
-            </div>
-        </div> 
+      <div className={`flex flex-col items-center`}>
+        {view === 'pc' ? <div className='w-5/6 h-[400px] overflow-hidden rounded-md'><Image alt={`${title} image`} onLoad={(e) => setImageHeight(e.target.clientHeight)} src={image} width={1000} height={1000} className={`transform hover:transform ${imageHeight > 400 ? 'hover:-translate-y-[calc(100%-400px)]' : ''} transition-transform duration-[5000ms] ease-linear`} /></div> : ''}
+        {view === 'info' ? <div className='w-5/6 h-[400px]'>
+          <h4 className='font-bold text-white mb-2 sm:mb-4 text-xl'>Tech Used:</h4>
+          <ul className='flex flex-row justify-evenly text-white mb-4 flex-wrap gap-1 sm:gap-2 '>
+            {techUsed.map(item => (
+              <li key={item} className='bg-opacity-45 ring-2 ring-inset p-3 rounded-md ring-green-400 ring-opacity-65 mb-4 text-xs md:text-md lg:text-lg'>{item}</li>
+            ))}
+          </ul>
+          <h4 className='font-bold text-white mb-2 sm:mb-4 text-xl'>Description:</h4>
+          <div className='h-[200px] overflow-auto p-1'>
+            <p className={`text-white text-xs sm:text-sm leading-normal md:text-md md:leading-loose min-`}>{description}</p>
+            {repo !== '' && liveSite !== '' && <ul className='flex flex-row justify-evenly'>
+              {repo !== '' && <li className='font-bold text-green-400'><Link href={repo}>Repo</Link></li>}
+              {liveSite !== '' && <li className='font-bold text-green-400'><Link href={liveSite}>Live Site</Link></li>}
+            </ul>
+            }
+          </div>
+
+        </div> : ''}
+
+        <div className='w-full flex flex-row justify-evenly mt-3'>
+          <div className={`rounded-full bg-black ${view === 'pc' ? 'bg-opacity-45 ring-2 ring-inset ring-green-400 ring-opacity-65' : 'bg-opacity-0'} p-3 transition-opacity`}> <IoIosDesktop size={35} color='white' className='cursor-pointer' onClick={() => setView('pc')} /> </div>
+          {mobileImage ? <div className={`rounded-full bg-black ${view === 'mobile' ? 'bg-opacity-45 ring-2 ring-inset ring-green-400 ring-opacity-65' : 'bg-opacity-0'} p-3 transition-opacity`}> <FaMobileAlt size={42} color='white' className='cursor-pointer' onClick={() => setView('mobile')} /> </div> : ''}
+          <div className={`rounded-full bg-black ${view === 'info' ? 'bg-opacity-45 ring-2 ring-inset ring-green-400 ring-opacity-65' : 'bg-opacity-0'} p-3 transition-opacity`}> <BsInfoLg size={35} color='white' className='cursor-pointer' onClick={() => setView('info')} /> </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -7,63 +7,81 @@ const projects = [
     title: 'Harmonford Marathon',
     image: '/harmonford_marathon_pc.png',
     techUsed: ['HTML', 'CSS', 'JavaScript'],
-    description: "As a keen runner I've spent a fair amount of time on marathon & half marathon website, so I thought I'd have a go at building my own. After using ReactJS & Tailwind CSS it was refreshing to go back to the basics to build this project."
+    description: "As a keen runner I've spent a fair amount of time on marathon & half marathon website, so I thought I'd have a go at building my own. After using ReactJS & Tailwind CSS it was refreshing to go back to the basics to build this project.",
+    repo: '',
+    liveSite: ''
   },
   {
     id: 2,
     title: 'LFC Clone Website',
     image: '/lfc_clone.png',
     techUsed: ['NextJS', 'ReactJS', 'NodeJS', 'TailwindCSS'],
-    description: 'This was the first website I built whilst learning NextJS - the official LFC website has a fairly simple design but with some interesting features to recreate.'
+    description: 'This was the first website I built whilst learning NextJS - the official LFC website has a fairly simple design but with some interesting features to recreate.',
+    repo: '',
+    liveSite: ''
   },
   {
     id: 3,
     title: 'CSS Visualised',
     image: '/css_visualised.png',
     techUsed: ['HTML', 'CSS', 'JavaScript'],
-    description: 'This website serves as a visual guide for understanding the flexbox and box shadow CSS properties, which are often challenging to grasp. Being able to toggle the values and observe the changes proved very helpful. Additionally, I added a drawing function (a basic version of paint). This was a result of me learning mouse events at the time, making the project a practical application of the concepts I was learning.'
+    description: 'This website serves as a visual guide for understanding the flexbox and box shadow CSS properties, which are often challenging to grasp. Being able to toggle the values and observe the changes proved very helpful. Additionally, I added a drawing function (a basic version of paint). This was a result of me learning mouse events at the time, making the project a practical application of the concepts I was learning.',
+    repo: '',
+    liveSite: ''
   },
   {
     id: 4,
     title: 'Invoice Generator',
     image: '/invoice_generator.png',
     techUsed: ['HTML', 'CSS', 'JavaScript', 'PDFJS'],
-    description: "While working for Essex Carers Network on a self-employed basis, I've needed to produce invoices. For the first few, I simply used online invoice generators but was always curious as to whether I could create my own. Overall, the project was quite simple; it just required some input fields and a way of generating a PDF. I wasn't familiar with how to do the second part, but Stack Overflow was very helpful and led me to find PDF.js, which had great documentation."
+    description: "While working for Essex Carers Network on a self-employed basis, I've needed to produce invoices. For the first few, I simply used online invoice generators but was always curious as to whether I could create my own. Overall, the project was quite simple; it just required some input fields and a way of generating a PDF. I wasn't familiar with how to do the second part, but Stack Overflow was very helpful and led me to find PDF.js, which had great documentation.",
+    repo: 'https://github.com/SamStrike98/Invoice-Generator',
+    liveSite: ''
   },
   {
     id: 5,
     title: 'Weather App',
     image: '/weather_app.png',
     techUsed: ['HTML', 'CSS', 'JavaScript', 'WeatherAPI'],
-    description: "This was the first project I built that consumes an API. In this case, it was a weather API that provided a three-day hourly forecast when given a Town, City, or Postcode of any location in the world. This project gave me a great understanding of fetching data and handling the JSON response."
+    description: "This was the first project I built that consumes an API. In this case, it was a weather API that provided a three-day hourly forecast when given a Town, City, or Postcode of any location in the world. This project gave me a great understanding of fetching data and handling the JSON response.",
+    repo: 'https://github.com/SamStrike98/Weather',
+    liveSite: ''
   },
   {
     id: 6,
     title: 'Blog Website',
     image: '/football_blog.png',
     techUsed: ['ReactJS', 'NextJS', 'NodeJS', 'Strapi'],
-    description: "For this project, I used Strapi (an open-source JavaScript-based headless CMS) as the 'backend'. This helped to further my experience with fetching, filtering, and displaying different types of data. On the frontend, I utilized Next.js, which allowed me to gain an understanding of Server-Side Rendering versus Client-Side Rendering and the effect this may have on performance."
+    description: "For this project, I used Strapi (an open-source JavaScript-based headless CMS) as the 'backend'. This helped to further my experience with fetching, filtering, and displaying different types of data. On the frontend, I utilized Next.js, which allowed me to gain an understanding of Server-Side Rendering versus Client-Side Rendering and the effect this may have on performance.",
+    repo: 'https://github.com/SamStrike98/blog',
+    liveSite: ''
   },
   {
     id: 7,
     title: 'The Trainer Experts',
     image: '/the_trainer_experts.png',
     techUsed: ['HTML', 'CSS', 'JavaScript'],
-    description: "The main aim of this project was to plan out the design and structure before starting to build it. Additionally, I thought it would be interesting to develop an e-commerce-style project."
+    description: "The main aim of this project was to plan out the design and structure before starting to build it. Additionally, I thought it would be interesting to develop an e-commerce-style project.",
+    repo: 'https://github.com/SamStrike98/Trainer-Experts',
+    liveSite: ''
   },
   {
     id: 8,
     title: 'Harmonford Estates',
     image: '/harmonford_estates.png',
     techUsed: ['ReactJS', 'NextJS', 'Strapi', 'TailwindCSS', 'DaisyUI'],
-    description: "Using Strapi as a headless CMS, this project allowed me to practice key skills such as fetching data, filtering data, pagination, and error handling."
+    description: "Using Strapi as a headless CMS, this project allowed me to practice key skills such as fetching data, filtering data, pagination, and error handling.",
+    repo: 'https://github.com/SamStrike98/harmonford_estates',
+    liveSite: 'https://harmonford-estates.vercel.app/'
   },
   {
     id: 9,
     title: 'Gifts By Breed',
     image: '/gifts_by_breed.png',
     techUsed: ['ReactJS', 'NextJS', 'TailwindCSS', 'MongoDB', 'Auth.js', 'Stripe'],
-    description: "This is the most complete project I've built to date, incorporating NextJS and TailwindCSS on the frontend, connecting via an API to a MongoDB database, and also making use of the Stripe API to collect payments and AuthJS for authentication and authorisation."
+    description: "This is the most complete project I've built to date, incorporating NextJS and TailwindCSS on the frontend, connecting via an API to a MongoDB database, and also making use of the Stripe API to collect payments and AuthJS for authentication and authorisation.",
+    repo: 'https://github.com/SamStrike98/gifts-by-breed',
+    liveSite: 'https://gifts-by-breed.vercel.app/'
   },
 ]
 
@@ -71,7 +89,7 @@ const ProjectSection = () => {
   return (
     <div className='flex flex-col lg:flex-row w-full items-center lg:items-start lg:justify-evenly flex-wrap gap-4'>
       {projects.map(project => (
-        <ProjectCard key={project.id} title={project.title} image={project.image} description={project.description} id={project.id} techUsed={project.techUsed} />
+        <ProjectCard key={project.id} title={project.title} image={project.image} description={project.description} id={project.id} techUsed={project.techUsed} repo={project.repo} liveSite={project.liveSite} />
       ))}
     </div>
   )
